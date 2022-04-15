49c851a51e5e31a409afe74df9e1922880559953
Szymon Miśtal <mistalzaq@gmail.com>
2022-04-15 12:30:52 +0200

    Add config setting to control data persistance

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,15 @@
 import * as vscode from "vscode";
 
+export enum PersistenceType {
+  NONE = "none",
+  DAY_TO_DAY = "day",
+  PERMAMENT = "permament",
+}
+
 export const Settings = {
   getTimeout: (): number =>
     vscode.workspace.getConfiguration("coding-timer").get("timeout") ?? 5,
+  getPersistance: (): PersistenceType =>
+    vscode.workspace.getConfiguration("coding-timer").get("persistance") ??
+    PersistenceType.DAY_TO_DAY,
 };
--- src/storage.ts
+++ src/storage.ts
@@ -1,16 +1,25 @@
 import * as vscode from "vscode";
+import { PersistenceType, Settings } from "./settings";
 import { Timer } from "./timer";
 import { ExtensionData } from "./types";
 
 const STORAGE_KEY = "coding-timer";
 
 function isDifferentByDay(d1: Date, d2: Date) {
-  return d1.getTime() - d2.getTime() >= 1000 * 60 * 60 * 24;
+  return (
+    d1.getDay() !== d2.getDay() ||
+    d1.getTime() - d2.getTime() >= 1000 * 60 * 60 * 24
+  );
 }
 
 export const Storage = {
   init: (context: vscode.ExtensionContext) => {
-    if (isDifferentByDay(new Date(), new Date(Storage.get(context).date))) {
+    const persistance = Settings.getPersistance();
+    if (
+      persistance === PersistenceType.NONE ||
+      (persistance === PersistenceType.DAY_TO_DAY &&
+        isDifferentByDay(new Date(), new Date(Storage.get(context).date)))
+    ) {
       Storage.reset(context);
     }
   },
